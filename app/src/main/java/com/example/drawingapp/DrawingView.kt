d84faa7e1f04040733de8b3fd86cb8fd98b03dcc
TSanni <tomas.sanni7@gmail.com>
2023-01-03 15:40:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -0,0 +1,111 @@
+package com.example.drawingapp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attributes: AttributeSet): View(context, attributes) {
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    private var mPaths = ArrayList<CustomPath>()
+
+    init {
+        setupDrawing()
+    }
+
+    private fun setupDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color,mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+//        mBrushSize = 20.toFloat()
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+//        var test = mCanvasBitmap.copy(Bitmap.Config.ARGB_8888, true)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+
+    //Change Canvas to Canvas? if fails
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for (path in mPaths) {
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path!!.color
+            canvas.drawPath(path ,mDrawPaint!!)
+        }
+
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> { //Pressing down on the screen
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_MOVE -> { //Dragging over the screen
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_UP -> { //Releasing touch
+                mPaths.add(mDrawPath!!)
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+
+            else -> {
+                return false
+            }
+        }
+        invalidate()
+
+        return true
+
+//        return super.onTouchEvent(event)
+
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float): Path() { //this class only usable in DrawingView class
+
+    }
+}
